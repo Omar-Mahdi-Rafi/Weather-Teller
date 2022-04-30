79ba6916d466942201cc0b70faf349ffcc72cd7d
OMAR MAHDI RAFI <97361722+Omar-Mahdi-Rafi@users.noreply.github.com>
2022-04-30 20:48:27 +0600

    Update Weather.test.js

--- TestCodes/Weather.test.js
+++ TestCodes/Weather.test.js
@@ -2,6 +2,6 @@ const Weather = require('./Weather')
 
 test('Takes only valid input, throws error otherwise', () => {
     expect(
-        12+1)
-        .toBe(13)
+        validateUserInput())
+        .toBe(false)
 })
